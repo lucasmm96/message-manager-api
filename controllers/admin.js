c05aaba5446909a796338b06c8d71a63f207fbb4
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-01-27 12:06:54 -0300

    change code return

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,7 +12,7 @@ exports.getMessageById = (req, res) => {
 exports.getMessageList = (req, res) => {
 	Message.find()
 		.then((data) => {
-			res.status(201).json(data);
+			res.status(200).json(data);
 		})
 		.catch((err) => console.log(err));
 };
