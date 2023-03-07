a6e550542c3d77422bea0c8a45fddc5f0abe012f
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-07 09:59:41 -0300

    cleanup userSchema

--- models/user.js
+++ models/user.js
@@ -5,36 +5,7 @@ const userSchema = new Schema({
 	username: { type: String, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
-	messages: [
-		{
-			id: { type: Schema.Types.ObjectId, ref: 'Message', required: true },
-			addedAt: { type: Date, required: true },
-			postedAt: { type: Date, required: false },
-		},
-	],
+	admin: { type: Object, required: true },
 });
-
-userSchema.methods.addMessage = function (messageId, addedAt, postedAt) {
-	const newMessage = {
-		id: messageId,
-		addedAt: addedAt,
-		postedAt: postedAt,
-	};
-	this.messages.push(newMessage);
-	return this.save();
-};
-
-userSchema.methods.updateMessage = function (MessageIndex, postedAt) {
-	this.messages[MessageIndex].postedAt = postedAt;
-	return this.save();
-};
-
-userSchema.methods.removeMessage = function (messageId) {
-	const udpdatedMessageList = this.messages.filter((selectedItem) => {
-		return selectedItem.id.toString() !== messageId.toString();
-	});
-	this.messages = udpdatedMessageList;
-	return this.save();
-};
 
 module.exports = mongoose.model('User', userSchema);
