a2f72f0324cf4d67cf6139b3598e5e7778e94046
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-04-28 13:49:53 -0300

    check auth

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin');
 const authController = require('../controllers/auth');
+const checkAuth = require('../middleware/check-auth');
 const isBodyArray = require('../middleware/isBodyArray');
 
 router.post('/auth/signup', authController.postSignup);
@@ -9,6 +10,9 @@ router.post('/auth/login', authController.postLogin);
 
 router.get('/message/list', adminController.getMessageList);
 router.get('/message/find', isBodyArray, adminController.getMessageById);
+
+router.use(checkAuth);
+
 router.post('/message/add', isBodyArray, adminController.postAddMessage);
 router.post('/message/update', isBodyArray, adminController.postUpdateMessage);
 router.post('/message/delete', isBodyArray, adminController.getDeleteMessage);
