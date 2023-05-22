d1d94c6ef278fb84991c60c3955b4d893a3e72a6
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2023-05-22 12:51:00 -0300

    .env variable for port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
     console.log("App running on port 3000");
   })
   .catch((err) => console.log(err));
