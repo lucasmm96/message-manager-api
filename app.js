c3563efad55b5ea0f929902fd7b31ac5655fa074
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2023-05-22 12:50:35 -0300

    prettier adjustments

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const adminRoute = require('./routes/admin');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const adminRoute = require("./routes/admin");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,14 +11,14 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(adminRoute);
 app.use((req, res) => {
-	res.status(404).json({ message: 'Route not found.' });
+  res.status(404).json({ message: "Route not found." });
 });
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		app.listen(3000);
-		console.log('App running on port 3000');
-	})
-	.catch((err) => console.log(err));
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(3000);
+    console.log("App running on port 3000");
+  })
+  .catch((err) => console.log(err));
