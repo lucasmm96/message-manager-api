a8c9822c52bab4eaa53c4b05d434d08632c5a08f
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2023-05-31 16:38:41 -0300

    add type field

--- models/pendingMessage/structure/head.js
+++ models/pendingMessage/structure/head.js
@@ -1,5 +1,6 @@
 module.exports = pendingMessageHead = {
   user: { type: String, required: true },
   action: { type: String, enum: ['Add', 'Update', 'Delete'], required: true },
+  type: { type: String, enum: ['Approval', 'Backup'], required: true },
 	status: { type: String, required: true, default: 'Pending' , enum: ['Pending', 'Accepted', 'Rejected']},
 };
