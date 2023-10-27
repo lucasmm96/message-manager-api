c6657e2ead8d15e360d5d5566fc1efef3718f31f
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2023-10-27 19:02:27 -0300

    order by `requestedAt`

--- controllers/admin.js
+++ controllers/admin.js
@@ -29,6 +29,7 @@ exports.getPendingMessageList = async (req, res) => {
           requesterName: '$userData.username',
         },
       },
+      { $sort: { requestedAt: 1 } },
       { $skip: parseInt(skip) },
       { $limit: parseInt(size) },
     ]);
